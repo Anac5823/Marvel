ce4ffd881b66d92eb061ced9b7252886c16ba0b4
Anac5823 <ana.c5823@gmail.com>
2022-12-15 22:19:46 +0000

    the registration screen was made, and the login screen, the infoplist was updated with the login storyboard as the main one, path to the password reset and registration screen

---
+++ Marvel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Marvel
+//
+//  Created by Ana Luiza on 12/13/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
