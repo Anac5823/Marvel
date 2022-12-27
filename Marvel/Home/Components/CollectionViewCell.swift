bfeddb2a6d4be6a5c263eb0e938e568760029c58
Anac5823 <ana.c5823@gmail.com>
2022-12-27 19:28:24 +0000

    introducing the components of the home screen and creating the details screen

---
+++ Marvel/Home/Components/CollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CollectionViewCell.swift
+//  Marvel
+//
+//  Created by Ana Luiza on 12/27/22.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ Marvel/Home/Components/MovieCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MovieCollectionViewCell.swift
+//  Marvel
+//
+//  Created by Ana Luiza on 12/27/22.
+//
+
+import UIKit
+
+class MovieCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
